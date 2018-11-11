5792f55962a5c77e799d709e3a59db94da7c7d36
adonispujols <adonispujols@users.noreply.github.com>
2018-11-11 14:54:26 -0500

    more work on turn based.

--- src/turn_actions.py
+++ src/turn_actions.py
@@ -10,9 +10,65 @@ def special_untap(game: game_mod.Game, first_player: player_mod.Player):
     game.untap_all_of_player(first_player.index())
     upkeep(game)
 
+# func upkeep(game):
+# 	game.step_or_phase = 1
+# 	game.get_active_player.gain_priority()
+#
+# func draw(game):
+# 	game.step_or_phase = 2
+# 	game.get_active_player().draw(1)
+# 	game.get_active_player.gain_priority()
+#
+# func pre_combat_phase_start(game):
+#     game.step_or_phase = 3
+#     game.get_active_player.gain_priority()
+#
+# func begin_combat(game):
+#     game.step_or_phase = 4
+#     game.get_active_player.gain_priority()
+#
+# func declare_attackers(game):
+#     game.step_or_phase = 5
+#     game.get_active_player.gain_priority()
+#
+# func declare_blockers(game):
+#     game.step_or_phase = 6
+#     game.get_active_player.gain_priority()
+#
+# func first_strike_damage(game):
+#     game.step_or_phase = 7
+#     game.get_active_player.gain_priority()
+#
+# func combat_damage(game):
+#     game.step_or_phase = 8
+#     game.get_active_player.gain_priority()
+#
+# func end_combat(game):
+#     game.step_or_phase = 9
+#     game.get_active_player.gain_priority()
+#
+# func post_combat_phase_start(game):
+#     game.step_or_phase = 10
+#     game.get_active_player.gain_priority()
+#
+# func end(game):
+#     game.step_or_phase = 11
+#     game.get_active_player.gain_priority()
+#
+# func cleanup(game):
+#     game.step_or_phase = 12
+#     untap(game)
+# func untap(game):
+# 	game.step_or_phase = 0
+# 	game.change_active_player_to_next()
+# 	game.untap_all_of_player(get_active_player().index())
+# 	upkeep(game)
+
 def untap(game):
     # TODO
-    pass
+    game.step_or_phase = 0
+    game.change_active_player_to_next()
+    game.untap_all_of_player(game.active_player().)
 
 def upkeep(game):
     # TODO
@@ -63,76 +119,11 @@ def cleanup(game):
     pass
 
 
-# TODO store as tuple!!:
-# # list holding method names as strings
-# const start_methods = ["upkeep", "draw",
-# 		"pre_combat_phase_start", "begin_combat", "begin_combat",
-# 		"declare_attackers", "first_strike_damage",
-# 		"combat_damage", "end_combat", "post_combat_phase_start",
-# 		"end", "cleanup", "untap"]
-#
+START_METHODS = (upkeep(), draw(), pre_combat(), begin_combat(),
+                 declare_attackers(), declare_blockers(), first_strike_damage(),
+                 combat_damage(), end_combat(), post_combat(), end(), cleanup(),
+                 untap())
 
-START_METHODS = (upkeep(), draw(), pre_combat(), begin_combat(), 
 
 def start_next_step_or_phase(step_or_phase, game):
-    # TODO
-    pass
-
-# func start_next_step_or_phase(step_or_phase, game):
-# 	call(start_methods[step_or_phase], game) # START ON UPKEEP, untap last
-#
-
-# func untap(game):
-# 	game.step_or_phase = 0
-# 	game.change_active_player_to_next()
-# 	game.untap_all_of_player(get_active_player().index())
-# 	upkeep(game)
-#
-# func upkeep(game):
-# 	game.step_or_phase = 1
-# 	game.get_active_player.gain_priority()
-#
-# func draw(game):
-# 	game.step_or_phase = 2
-# 	game.get_active_player().draw(1)
-# 	game.get_active_player.gain_priority()
-#
-# func pre_combat_phase_start(game):
-#     game.step_or_phase = 3
-#     game.get_active_player.gain_priority()
-#
-# func begin_combat(game):
-#     game.step_or_phase = 4
-#     game.get_active_player.gain_priority()
-#
-# func declare_attackers(game):
-#     game.step_or_phase = 5
-#     game.get_active_player.gain_priority()
-#
-# func declare_blockers(game):
-#     game.step_or_phase = 6
-#     game.get_active_player.gain_priority()
-#
-# func first_strike_damage(game):
-#     game.step_or_phase = 7
-#     game.get_active_player.gain_priority()
-#
-# func combat_damage(game):
-#     game.step_or_phase = 8
-#     game.get_active_player.gain_priority()
-#
-# func end_combat(game):
-#     game.step_or_phase = 9
-#     game.get_active_player.gain_priority()
-#
-# func post_combat_phase_start(game):
-#     game.step_or_phase = 10
-#     game.get_active_player.gain_priority()
-#
-# func end(game):
-#     game.step_or_phase = 11
-#     game.get_active_player.gain_priority()
-#
-# func cleanup(game):
-#     game.step_or_phase = 12
-#     untap(game)
+    START_METHODS[step_or_phase](game)
