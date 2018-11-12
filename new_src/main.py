7b069ff8c5d8da5137cfd7c769cbe534a79312e3
adonispujols <adonispujols@users.noreply.github.com>
2018-11-12 18:22:07 -0500

    just before adding support for ai vs ai

--- new_src/main.py
+++ new_src/main.py
@@ -5,18 +5,39 @@ from new_src import deck
 from new_src import card as card_mod
 from new_src import hand
 
-# TODO keep code compatible code with multiplayer!:
-debug = True
 
-# player "1", the user, has index = 0, or nth player - 1
-# player "2", or the ai (unless debug is on), has index = 1
-players = [player_mod.Player(), player_mod.Player()]
+# TODO Always let code fail gracefully on invalid input (if reversible)!
+# XXX Maintain a complete, solid CLI to depend on during GUI dev.
+# ^ This is ALWAYS our main focus, with ports made to GUI when ready.
+# methods/classes for setting up game
+# (before going through official "Starting the Game" steps [CR 103])
+def init_battlefield():
+    for player in range(len(players)):
+        battlefield.append([])
 
-players[0].deck = deck.Deck()
-players[1].deck = deck.Deck()
-# ^ XXX hard setting attributes is not ideal. ( we'll refractor once done)
+
+class StepOrPhase:
+    # again, awkward creation of object
+    # XXX seems like we need to collect these into a "game" object or so
+    # ^ XXX HOWEVER, don't assume game should inherit the other methods!!
+    def __init__(self):
+        # So far this is hard set, can it be better?
+        self.index = 0
+
+    def get_index(self):
+        return self.index
 
 
+# utility methods
+def print_hand_and_decks():
+    # some objects have __repr__ defined (to simplify printing)
+    # XXX Use ID when comparing objects (as you should)
+    for i, player in enumerate(players):
+        print("P" + str(i + 1), "HAND:\n", player.hand,
+              "\nP" + str(i + 1), "DECK:\n", player.deck)
+
+
+# methods for officially "Starting the Game" [CR 103], in corresponding order
 def fill_decks():
     # fill each deck with 60 cards
     for x in range(60):
@@ -31,16 +52,40 @@ def shuffle_all():
         player.deck.shuffle()
 
 
-def player_choose_first(index):
-    print("P" + str(index + 1), ", who goes first?")
+def choose_first_player(index):
+    print("P" + str(index + 1) + ", who goes first?")
+    # XXX ALWAYS define funcs locally they'll ONLY be used in that context!
+    # ask the user for input
+    def user_chooses_first_player():
+        # must be an int and refer to an actual player
+        while True:
+            try:
+                choice = int(input("Player #: "))
+            except ValueError:
+                print("ERROR: Invalid int")
+            else:
+                if 1 <= choice <= len(players):
+                    first = int(choice) - 1
+                    break
+                else:
+                    print("ERROR: Invalid Player #")
+
+    # ask player_1, by default is user
     if index == 0:
-        # ask player_1, or user, for input
-        choice = input("Player Int: ")
-        first = int(choice) - 1
+
     elif debug:
         # user controls ai, ask for input like above
-        choice = input("Player Int: ")
-        first = int(choice) - 1
+        while True:
+            try:
+                choice = int(input("Player #: "))
+            except ValueError:
+                print("ERROR: Invalid int")
+            else:
+                if 1 <= choice <= len(players):
+                    first = int(choice) - 1
+                    break
+                else:
+                    print("ERROR: Invalid Player #")
     else:
         # ai is making choice (by default, chooses itself)
         first = index
@@ -54,46 +99,7 @@ def initial_draw():
             player.draw()
 
 
-fill_decks()
-shuffle_all()
-
-
-
-
-# TODO continue adding AI options in future!
-# XXX in a match, loser of last game chooses
-# randrange is [start, stop) (exclusive)
-first_player = player_choose_first(random.randrange(len(players)))
-
-players[0].hand = hand.Hand()
-players[1].hand = hand.Hand()
-
-
-
-initial_draw()
-
-
-def print_hand_and_decks():
-    # some objects have __repr__ defined (to simplify printing)
-    # XXX Use ID when comparing objects (as you should)
-    for i, player in enumerate(players):
-        print("P", i, "HAND:\n", player.hand, "\nP", i, "DECK:\n", player.deck)
-
-
-print_hand_and_decks()
-
-# cards in battlefield[player_index] pertain to that player
-battlefield: typing.List[typing.List[card_mod.Card]] = []
-
-
-def create_battlefield():
-    for player in range(len(players)):
-        battlefield.append([])
-
-
-create_battlefield()
-
-
+# methods/classes to use during game
 # XXX could definitely optimize this AND SIMILAR (however, clarity is key atm)
 def active_index():
     for i, player in enumerate(players):
@@ -111,23 +117,6 @@ def untap_all_of_active():
         card.untap()
 
 
-# TODO need to take into account actions taken in between passes!
-# XXX hacky solution, of course, but better than globals!
-# Again, the 50% > 99%, cause the 50% actually exists!
-class Passes:
-    def __init__(self):
-        self.passes = 0
-
-    def inc(self):
-        self.passes += 1
-
-    def count(self):
-        return self.passes
-
-
-passes = Passes()
-
-
 def give_player_priority(player_index):
     if passes.count() != len(players):
         if player_index == 0:
@@ -139,40 +128,51 @@ def give_player_priority(player_index):
                 give_player_priority(player_index + 1)
         elif debug:
             # ask user controlling ai for input (same as above)
-            choice = input("P" + str(player_index + 1) + " Press enter to pass")
+            choice = input("P" + str(player_index + 1) + ", Press enter to pass")
             if not choice:
                 passes.inc()
                 give_player_priority((player_index + 1) % len(players))
+        else:
+            # TODO continue adding AI options in future!
+            pass
     # "passed in succession" <- with no care about actions
     else:
-        start_next_step_or_phase(step_or_phase)
+        # MUST RESET PASSES (else we're stuck in infinite loop)
+        passes.reset()
+        start_next_step_or_phase(step_or_phase.index)
 
 
-# again, awkward creation of object
-# TODO seems like we need to collect these into a "game" object or so
-# ^ XXX HOWEVER, don't assume game should inherit the other methods!!
-class StepOrPhase:
+class Passes:
+    # TODO need to take into account actions taken in between passes!
+    # XXX hacky solution, of course, but better than globals!
+    # Again, the 50% > 99%, cause the 50% actually exists!
     def __init__(self):
-        # So far this is hard set, can it be better?
-        self.index = 0
+        self.passes = 0
 
-    def get_index(self):
-        return self.index
+    def inc(self):
+        self.passes += 1
+
+    def reset(self):
+        self.passes = 0
+
+    def count(self):
+        return self.passes
 
 
-step_or_phase = StepOrPhase()
-
+# methods/classes related to (specifically) turn based actions
 def start_next_step_or_phase(index):
     START_METHODS[index]()
 
 
-def special_untap():
+def first_untap_of_game():
     print("Start of First Untap Step")
     step_or_phase.index = 0
     # ^ XXX evil sets? (along with rest of step_or_phase.index = x)
     # at start, no one is active, so we must directly make first player active.
     # set 1st player to active player
     players[first_player].make_active()
+    print("Active Player:", active_index() + 1)
+    print("TBA: Untap all")
     untap_all_of_active()
     upkeep()
 
@@ -187,8 +187,9 @@ def untap():
     # XXX caling active_index is slightly in efficient, but HEY,
     # ^ there might be a corner case we need to cover
     # ALWAYS FAVOR SECURITY/CLARITY OVER EFFICIENCY (to reasonable limits)
-    print("Active Player:", active_index())
-    print("Untap Step: Untap")
+    print("Active Player:", active_index() + 1)
+    # TBA = "Turn-Based Action", SBA = "State-Based Action"
+    print("TBA: Untap all")
     untap_all_of_active()
     upkeep()
 
@@ -200,11 +201,12 @@ def upkeep():
 
 
 def draw():
-    # TODO must skip if 1st player's 1st draw (unless free for all)
+    # TODO must skip if 1st player's 1st draw (if 1v1 or 2-headed giant)
     print("Start of Draw Step")
     step_or_phase.index = 2
-    print("Draw step: Draw")
+    print("TBA: Draw")
     active_player().draw()
+    print_hand_and_decks()
     give_player_priority(active_index())
 
 
@@ -272,19 +274,44 @@ def cleanup():
     untap()
 
 
+# TODO keep code compatible code with multiplayer + ai vs ai!:
+
+# initializations (for set up)
+debug = True  # user controls AI, if true
+ai_only = False  # AI controls all players (including player 1)
+# player "1", the user y default, has index = 0, or nth player - 1
+# player "2", or the ai (unless debug is on), has index = 1
+players = [player_mod.Player(), player_mod.Player()]
+# XXX hard setting attributes is not ideal. ( we'll refractor once done)
+players[0].deck = deck.Deck()
+players[0].hand = hand.Hand()
+players[1].deck = deck.Deck()
+players[1].hand = hand.Hand()
+fill_decks()
+# cards in battlefield[player_index] pertain to that player
+battlefield: typing.List[typing.List[card_mod.Card]] = []
+init_battlefield()
+passes = Passes()
+step_or_phase = StepOrPhase()
 START_METHODS = (upkeep, draw, pre_combat, begin_combat, declare_attackers,
                  declare_blockers, first_strike_damage, combat_damage,
                  end_combat, post_combat, end, cleanup, untap)
 
+# Starting the game [CR 103]
+shuffle_all()
+# XXX in a match, loser of last game chooses
+# randrange is [start, stop) (exclusive)
+first_player = choose_first_player(random.randrange(len(players)))
+initial_draw()
+print_hand_and_decks()
+first_untap_of_game()
 
-# TEST: 10 MINUTES HARD CODING QUICK COMMENTS LET'S GO!
+
+# 10 MINUTES HARD CODING QUICK COMMENTS LET'S GO!
 
 # you can only do stuff when game explicitly tells you you can****
 # you CAN'T affect ai doing stuff. you won't have the option to do ANYTHING!
-# we keep track of player choosing (what to do)
-# based on this, we give the option to play stuff relevant to that player!
-# XXX ^ need to handle times where you CAN play one of your opponents cards
-# XXX ^ how about playing from a different zone???
+# XXX need to handle times where you CAN play one of your opponents cards
 
 # import abc
 #
