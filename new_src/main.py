dd51ee45788fee17ade535d3b33f427809e3a271
adonispujols <adonispujols@users.noreply.github.com>
2018-11-13 20:20:04 -0500

    broadened scope of private vars to be STRICTLY internal

--- new_src/main.py
+++ new_src/main.py
@@ -5,12 +5,14 @@ from new_src import deck
 from new_src import card as card_mod
 from new_src import hand
 from new_src import stack as stack_mod
+from new_src import game as game_mod
 
 
 # XXX Don't forget about Planechase! Super fun!
 # XXX NEVER "SAFE" DELETE: It makes you *think* you don't need it, but you might of!
 # XXX Assume private fields/methods, then make public if needed by OTHER objects
 # ^ "Private" = strictly internal. I.e., NEVER USED outside the class or module/file
+# ^ THIS INCLUDES INIT! Outside access means it still isn't *STRICTLY internal*!
 # XXX Never use straight getters/setters <- use properties!
 # XXX Last design failed because you thought too much about "what" and not "how"
 # ^ Don't guess ahead of time what's needed! Find out by trying to do it!
@@ -24,38 +26,13 @@ from new_src import stack as stack_mod
 
 # methods/classes for setting up game
 # (before going through official "Starting the Game" steps [CR 103])
-def init_battlefield():
-    for player in range(len(players)):
-        battlefield.append([])
-
-
-class StepOrPhase:
-    # again, awkward creation of object
-    # XXX seems like we need to collect these into a "game" object or so
-    # ^ XXX HOWEVER, don't assume game should inherit the other methods!!
-    def __init__(self):
-        # So far this is hard set, can it be better?
-        self.index = 0
-
-    def get_index(self):
-        return self.index
-
-
-# utility methods
-def print_hand_and_decks():
-    # some objects have __repr__ defined (to simplify printing)
-    # XXX Use ID when comparing objects (as you should)
-    for i, player in enumerate(players):
-        print("P" + str(i + 1), "HAND:\n", player.hand,
-              "\nP" + str(i + 1), "DECK:\n", player.deck)
-
-
-def print_hand(index):
-        print("P" + str(index + 1), "HAND:\n", players[index].hand)
+def init_battlefield(game):
+    for player in range(len(game.players)):
+        game._battlefield.append([])
 
 
 # methods for officially "Starting the Game" [CR 103], in corresponding order
-def fill_decks():
+def fill_decks(game):
     # fill each deck with 60 cards
     for x in range(60):
         # each card must be a new, separate object,
@@ -64,12 +41,12 @@ def fill_decks():
         players[1].deck.add_top(card_mod.Card("two"))
 
 
-def shuffle_all():
+def shuffle_all(game):
     for player in players:
         player.deck.shuffle()
 
 
-def choose_first_player(index):
+def choose_first_player(game, index):
     print("P" + str(index + 1) + ", who goes first?")
 
     # XXX ALWAYS define funcs locally they'll ONLY be used in that context!
@@ -102,248 +79,33 @@ def choose_first_player(index):
     return first
 
 
-def initial_draw():
+def initial_draw(game):
     for player in players:
         for n in range(player.max_hand_size()):
             player.draw()
 
 
-# methods/classes to use during game
-# XXX could definitely optimize this AND SIMILAR (however, clarity is key atm)
-def active_index():
-    for i, player in enumerate(players):
-        if player.is_active():
-            return i
-
-
-def active_player():
-    return players[active_index()]
-
-
-# untap all of active player's permanents
-def untap_all_of_active():
-    for card in battlefield[active_index()]:
-        card.untap()
-
-
-def give_player_priority(index):
-    if passes.count() != len(players):
-        # ask the user for input
-        def user_has_priority():
-            while True:
-                # splits choice into string list, separated by whitespaces
-                choice = input("P" + str(index + 1) + ": ").split()
-                # TODO here is where we add more choices for player
-                # ^ either actions requiring priority (play, activate, pass, etc)
-                # ^ OR ability to look at game state
-                # ^^ XXX could organize ALL input asks such that:
-                # ^^ user may always look at the board state before a choice
-                # if just pressed enter (entered no input)
-                if not choice:
-                    passes.inc()
-                    give_player_priority((index + 1) % len(players))
-                    break
-                # list of options available (debugging or not)
-                # TODO implement user-limited commands (no debug)
-                # ^ normally, user's knowledge of game is limited
-                # ^ he can't just randomly search through hands, decks, etc
-                # ^ EX: hand-self prints own hand of player
-                # TODO play(card)
-                elif choice[0] == "TODO":
-                    pass
-                elif debug:
-                    # list of options available only if debugging
-                    if choice[0] == "hand":
-                        # XXX make a general "valid player index" method?
-                        try:
-                            # index<_n> are just aliases for player index
-                            index_1 = int(choice[1]) - 1
-                        except ValueError:
-                            print("ERROR: Invalid integer")
-                        except IndexError:
-                            print("ERROR: Need 1 Player # parameter, given 0")
-                        else:
-                            # XXX Apply EAFP ONLY when validating input, NOT LOGIC!
-                            # ^ I.e., checking for int/params is FINE! BUT we
-                            # ^ can't allow for irreversible game states by allowing
-                            # ^ invalid actions to run until error is caught!!!
-                            # - Essentially: Preemptively stop illegal game states
-                            # - from existing!
-                            if 0 <= index_1 < len(players):
-                                print_hand(index_1)
-                            else:
-                                print("ERROR: Invalid Player #")
-                else:
-                    print("ERROR: Invalid input")
-        if index == 0:
-            if not ai_only:
-                user_has_priority()
-            else:
-                # TODO continue adding AI options in future!
-                # ai is making choice
-                pass
-        else:
-            if debug:
-                user_has_priority()
-            else:
-                # ai is making choice
-                pass
-    # TODO need to take into account actions taken in between passes!
-    # "passed in succession"
-    else:
-        # MUST RESET PASSES (else we're stuck in infinite loop)
-        passes.reset()
-        start_next_step_or_phase(step_or_phase.index)
-
-
-class Passes:
-    # XXX hacky solution, of course, but better than globals!
-    # Again, the 50% > 99%, cause the 50% actually exists!
-    def __init__(self):
-        self.passes = 0
-
-    def inc(self):
-        self.passes += 1
-
-    def reset(self):
-        self.passes = 0
-
-    def count(self):
-        return self.passes
-
-
-# methods/classes related to (specifically) turn based actions
-def start_next_step_or_phase(index):
-    START_METHODS[index]()
-
-
-# XXX try to make this share code with untap
-def first_untap_of_game():
-    print("Start of First Untap Step")
-    step_or_phase.index = 0
-    # ^ XXX evil sets? (along with rest of step_or_phase.index = x)
-    # at start, no one is active, so we must directly make 1st player active.
-    players[first_player].make_active()
-    print("Active Player:", active_index() + 1)
-    print("TBA: Untap all")
-    untap_all_of_active()
-    upkeep()
-
-
-def untap():
-    print("Start of Untap Step")
-    step_or_phase.index = 0
-    # change active player to the next
-    prev_active_index = active_index()
-    active_player().make_inactive()
-    players[(prev_active_index + 1) % len(players)].make_active()
-    # XXX calling active_index again is slightly inefficient, but HEY,
-    # ^ there might be a corner case we need to cover
-    # ALWAYS FAVOR SECURITY/CLARITY OVER EFFICIENCY (to reasonable limits)
-    print("Active Player:", active_index() + 1)
-    # TBA = "Turn-Based Action", SBA = "State-Based Action"
-    print("TBA: Untap all")
-    untap_all_of_active()
-    upkeep()
-
-
-def upkeep():
-    print("Start of Upkeep Step")
-    step_or_phase.index = 1
-    give_player_priority(active_index())
-
-
-def draw():
-    # TODO must skip if 1st player's 1st draw (if 1v1 or 2-headed giant)
-    print("Start of Draw Step")
-    step_or_phase.index = 2
-    print("TBA: Draw")
-    active_player().draw()
-    print_hand_and_decks()
-    give_player_priority(active_index())
-
-
-def pre_combat():
-    print("Start of Precombat Main Phase")
-    step_or_phase.index = 3
-    give_player_priority(active_index())
-
-
-def begin_combat():
-    print("Start of Beginning of Combat Step")
-    step_or_phase.index = 4
-    give_player_priority(active_index())
-
-
-def declare_attackers():
-    print("Start of Declare Attackers Step")
-    # TODO need to skip to end if no attackers declared
-    step_or_phase.index = 5
-    give_player_priority(active_index())
-
-
-def declare_blockers():
-    print("Start of Declare Blockers Step")
-    step_or_phase.index = 6
-    give_player_priority(active_index())
-
-
-def first_strike_damage():
-    print("Start of First Strike Damage Step")
-    # TODO need to skip to combat damage if no creatures wih first strike
-    # ^ on either side of the field
-    step_or_phase.index = 7
-    give_player_priority(active_index())
-
-
-def combat_damage():
-    print("Start of Combat Damage Step")
-    step_or_phase.index = 8
-    give_player_priority(active_index())
-
-
-def end_combat():
-    print("Start of End of Combat Step")
-    step_or_phase.index = 9
-    give_player_priority(active_index())
-
-
-def post_combat():
-    print("Start of Postcombat Main Phase")
-    step_or_phase.index = 10
-    print("Step or Phase:", step_or_phase.index)
-    give_player_priority(active_index())
-
-
-def end():
-    print("Start of End Step")
-    step_or_phase.index = 11
-    give_player_priority(active_index())
-
-
-def cleanup():
-    print("Start of Cleanup Step")
-    step_or_phase.index = 12
-    untap()
-
+# XXX Constantly peeking into game implies this probably should be done by it
 
 # initializations (for set up)
+game = game_mod.Game()
 # user controls all players (including AI), if true
-debug = True
+game._debug = True
 # AI controls all players (including player 1), if true
-ai_only = False
+game._ai_only = False
+# cards in battlefield[player_index] pertain to that player
+game.battlefield = []
+init_battlefield(game)
 # player 1, the user by default, has index = 0
 # player 2+, the ai by default, has index = nth player - 1
-players = [player_mod.Player(), player_mod.Player()]
+game.players = [player_mod.Player(), player_mod.Player()]
 # XXX hard setting attributes is not ideal. ( we'll refactor once done)
-players[0].deck = deck.Deck()
-players[0].hand = hand.Hand()
-players[1].deck = deck.Deck()
-players[1].hand = hand.Hand()
-fill_decks()
-# cards in battlefield[player_index] pertain to that player
-battlefield: typing.List[typing.List[card_mod.Card]] = []
-init_battlefield()
+game.players[0].deck = deck.Deck()
+game.players[0].hand = hand.Hand()
+game.players[1].deck = deck.Deck()
+game.players[1].hand = hand.Hand()
+fill_decks(game)
+
 passes = Passes()
 step_or_phase = StepOrPhase()
 START_METHODS = (upkeep, draw, pre_combat, begin_combat, declare_attackers,
