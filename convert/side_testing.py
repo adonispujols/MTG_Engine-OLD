117ff0a4c013f0a9f3ff6ad20e3e6db860cf248b
adonispujols <adonispujols@users.noreply.github.com>
2018-11-20 00:27:59 -0500

    taking time to update/install packages in venv

--- convert/side_testing.py
+++ convert/side_testing.py
@@ -13,16 +13,20 @@ def test():
 def test_2(index):
     print("test 2 done. i: ", index)
 
-
 root = tk.Tk()
 root.title("title")
 button = tk.Button(root, text="button", command=test)
 button.grid()
-
 # TODO USE .config(command="") TO REMOVE COMMANDS FROM BUTTONS, NOT UNBIND!
 # button.config(command="")
 # TODO DON'T REMOVE COMMAND/BUTTON IF YOU WANT IT TO STAY. DISABLE BUTTON, INSTEAD!
-button.config(state=tk.DISABLED)
+# button.config(state=tk.DISABLED)
+
+
+pic = tk.PhotoImage(file="test_pic.png")
+test_button = tk.Button(root, image=pic)
+test_button.grid()
+
 # lift and topmost = true automatically brings window to top
 # topmost = false so it isn't stuck on top
 root.lift()
